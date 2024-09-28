2901241a49cddf0b592f5042c643a1e79d3f9354
KatatsumuriPan <katatsumuri.pan@gmail.com>
2024-09-28 13:01:40 +0900

    format

--- src/main/java/kpan/not_enough_oxygen/proxy/CommonProxy.java
+++ src/main/java/kpan/not_enough_oxygen/proxy/CommonProxy.java
@@ -4,11 +4,11 @@ import net.minecraft.item.Item;
 
 @SuppressWarnings("unused")
 public class CommonProxy {
-	public void registerOnlyClient() { }
+    public void registerOnlyClient() { }
 
-	public boolean hasClientSide() { return false; }
+    public boolean hasClientSide() { return false; }
 
-	public void registerSingleModel(Item item, int meta, String id) { }
+    public void registerSingleModel(Item item, int meta, String id) { }
 
-	public void registerMultiItemModel(Item item, int meta, String filename, String id) { }
+    public void registerMultiItemModel(Item item, int meta, String filename, String id) { }
 }
