8d93f710bd138de6046bbecf0b73e893e8d239d2
KatatsumuriPan <katatsumuri.pan@gmail.com>
2024-09-28 12:59:41 +0900

    format

--- src/main/java/kpan/not_enough_oxygen/proxy/ClientProxy.java
+++ src/main/java/kpan/not_enough_oxygen/proxy/ClientProxy.java
@@ -9,23 +9,23 @@ import net.minecraftforge.client.model.ModelLoader;
 @SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
 
-	@SuppressWarnings("RedundantMethodOverride")
-	@Override
-	public void registerOnlyClient() {
-		//MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
-	}
+    @SuppressWarnings("RedundantMethodOverride")
+    @Override
+    public void registerOnlyClient() {
+        // MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
+    }
 
-	@Override
-	public boolean hasClientSide() { return true; }
+    @Override
+    public boolean hasClientSide() { return true; }
 
 
-	@Override
-	public void registerSingleModel(Item item, int meta, String id) {
-		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
-	}
+    @Override
+    public void registerSingleModel(Item item, int meta, String id) {
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+    }
 
-	@Override
-	public void registerMultiItemModel(Item item, int meta, String filename, String id) {
-		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(ModTagsGenerated.MODID, filename), id));
-	}
+    @Override
+    public void registerMultiItemModel(Item item, int meta, String filename, String id) {
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(ModTagsGenerated.MODID, filename), id));
+    }
 }
